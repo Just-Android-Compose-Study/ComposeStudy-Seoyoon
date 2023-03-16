a87267e6a1b3a1bd5cd0d0a8b2b6f842f631aa31
seoyoon513 <syoonlee513@gmail.com>
2023-03-16 16:44:10 +0900

    [feat] #9 viewModelDemo 예제 구현

--- Chapter05/app/src/main/java/com/syoon/compose/chapter05/MainActivity.kt
+++ Chapter05/app/src/main/java/com/syoon/compose/chapter05/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -18,15 +20,33 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import java.util.*
 import kotlin.random.Random
+
+class MyViewModel : ViewModel() {
+
+    private val _text: MutableLiveData<String> =
+        MutableLiveData<String>("Hello #3")
+
+    val text: LiveData<String>
+        get() = _text
+
+    fun setText(value: String) {
+        _text.value = value
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             //TextFieldDemo1()
-            FlowOfEventsDemo()
+            //FlowOfEventsDemo()
+            ViewModelDemo()
         }
     }
 }
@@ -227,6 +247,45 @@ fun TemperatureRadioButton(
             text = stringResource(resId),
             modifier = Modifier
                 .padding(start = 8.dp)
+        )
+    }
+}
+
+/**
+ * ViewModelDemo 예제
+ */
+@Composable
+@Preview
+fun ViewModelDemo() {
+    val viewModel: MyViewModel = viewModel()
+    val state1 = remember {
+        mutableStateOf("Hello #1")
+    }
+    val state2 = rememberSaveable {
+        mutableStateOf("Hello #2")
+    }
+    val state3 = viewModel.text.observeAsState()
+    state3.value?.let {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            MyTextField(state1) { state1.value = it }
+            MyTextField(state2) { state2.value = it }
+            MyTextField(state3) {
+                viewModel.setText(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun MyTextField(
+    value: State<String?>,
+    onValueChange: (String) -> Unit
+) {
+    value.value?.let {
+        TextField(
+            value = it,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth()
         )
     }
 }
