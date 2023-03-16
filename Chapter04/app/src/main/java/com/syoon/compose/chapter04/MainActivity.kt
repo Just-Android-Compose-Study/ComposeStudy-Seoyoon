1ac3889bc0a5f097c2b49fe098bedf2816079914
seoyoon513 <syoonlee513@gmail.com>
2023-03-16 15:52:01 +0900

    [feat] #7 기본 구성 요소 조합 예제

--- Chapter04/app/src/main/java/com/syoon/compose/chapter04/MainActivity.kt
+++ Chapter04/app/src/main/java/com/syoon/compose/chapter04/MainActivity.kt
@@ -3,41 +3,116 @@ package com.syoon.compose.chapter04
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.syoon.compose.chapter04.ui.theme.Chapter04Theme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Chapter04Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            PredefinedLayoutsDemo()
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PredefinedLayoutsDemo() {
+    val red = remember { mutableStateOf(true) }
+    val green = remember { mutableStateOf(true) }
+    val blue = remember { mutableStateOf(true) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        CheckboxWithLabel(
+            label = stringResource(id = R.string.red),
+            state = red
+        )
+        CheckboxWithLabel(
+            label = stringResource(id = R.string.green),
+            state = green
+        )
+        CheckboxWithLabel(
+            label = stringResource(id = R.string.blue),
+            state = blue
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .background(Color.LightGray)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Cyan)
+            ) {
+
+            }
+
+            Text(
+                text = "테스트",
+                modifier = Modifier.padding(16.dp),
+            )
+
+            if (red.value) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Red)
+                )
+            }
+            if (green.value) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
+                        .background(Color.Green)
+                )
+            }
+            if (blue.value) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(64.dp)
+                        .background(Color.Blue)
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Chapter04Theme {
-        Greeting("Android")
+fun CheckboxWithLabel(label: String, state: MutableState<Boolean>) { //다른 컴포저블 함수 재구성
+    Row(
+        modifier = Modifier.clickable {
+            state.value = !state.value
+        }, verticalAlignment = Alignment.CenterVertically //박스와 텍스트 수직 정렬 맞춤
+    ) {
+        Checkbox(
+            checked = state.value,
+            onCheckedChange = {
+                state.value = it
+            }
+        )
+        Text(
+            text = label,
+            modifier = Modifier.padding(start = 8.dp)
+        )
     }
 }
