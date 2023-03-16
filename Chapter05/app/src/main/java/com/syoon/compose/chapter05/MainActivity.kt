d777851c117fdec60451fddcf96c984b9e836892
seoyoon513 <syoonlee513@gmail.com>
2023-03-16 16:39:22 +0900

    [feat] #9 flowEventDemo 예제 구현

--- Chapter05/app/src/main/java/com/syoon/compose/chapter05/MainActivity.kt
+++ Chapter05/app/src/main/java/com/syoon/compose/chapter05/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.syoon.compose.chapter05
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import java.util.*
 import kotlin.random.Random
 
@@ -21,7 +25,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TextFieldDemo1()
+            //TextFieldDemo1()
+            FlowOfEventsDemo()
         }
     }
 }
@@ -84,4 +89,144 @@ fun SimpleStatelessComposable1() {
 @Composable
 fun SimpleStatelessComposable2(text: State<String>) {
     Text(text = text.value)
+}
+
+/**
+ * flowEventDemo 예제
+ */
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+@Preview
+fun FlowOfEventsDemo() {
+    val strCelsius = stringResource(id = R.string.celsius)
+    val strFahrenheit = stringResource(id = R.string.fahrenheit)
+    val temperature = remember { mutableStateOf("") }
+
+    // textField에 값이 들어갈 때마다 재구성 -> 라디오버튼 선택 값 기억
+    val scale = remember { mutableStateOf(R.string.celsius) }
+    //val scale = mutableStateOf(R.string.celsius)
+
+    Log.d("scale", "$scale")
+    var convertedTemperature by remember { mutableStateOf(Float.NaN) }
+    val calc = {
+        val temp = temperature.value.toFloat()
+        convertedTemperature = if (scale.value == R.string.celsius)
+            (temp * 1.8F) + 32F
+        else
+            (temp - 32F) / 1.8F
+    }
+    val result = remember(convertedTemperature) {
+        if (convertedTemperature.isNaN())
+            ""
+        else
+            "${convertedTemperature}${
+                if (scale.value == R.string.celsius)
+                    strFahrenheit
+                else strCelsius
+            }"
+    }
+    val enabled = temperature.value.isNotBlank()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TemperatureTextField(
+            temperature = temperature,
+            modifier = Modifier.padding(bottom = 16.dp),
+            callback = calc
+        )
+        TemperatureScaleButtonGroup(
+            selected = scale,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Button(
+            onClick = calc,
+            enabled = enabled
+        ) {
+            Text(text = stringResource(id = R.string.convert))
+        }
+        if (result.isNotEmpty()) {
+            Text(
+                text = result,
+                style = MaterialTheme.typography.h3
+            )
+        }
+    }
+}
+
+@Composable
+fun TemperatureTextField(
+    temperature: MutableState<String>,
+    modifier: Modifier = Modifier,
+    callback: () -> Unit
+) {
+    TextField(
+        value = temperature.value,
+        onValueChange = {
+            temperature.value = it
+        },
+        // hint
+        placeholder = {
+            Text(text = stringResource(id = R.string.placeholder))
+        },
+        modifier = modifier,
+        keyboardActions = KeyboardActions(onAny = {
+            callback()
+        }),
+        // 키보드 옵션 구성 선택
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Done
+        ),
+        singleLine = true
+    )
+}
+
+@Composable
+fun TemperatureScaleButtonGroup(
+    selected: MutableState<Int>,
+    modifier: Modifier = Modifier
+) {
+    val sel = selected.value
+    val onClick = { resId: Int -> selected.value = resId }
+    Row(modifier = modifier) {
+        TemperatureRadioButton(
+            selected = sel == R.string.celsius,
+            resId = R.string.celsius,
+            onClick = onClick
+        )
+        TemperatureRadioButton(
+            selected = sel == R.string.fahrenheit,
+            resId = R.string.fahrenheit,
+            onClick = onClick,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun TemperatureRadioButton(
+    selected: Boolean,
+    resId: Int,
+    onClick: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        RadioButton(
+            selected = selected,
+            onClick = {
+                onClick(resId)
+            }
+        )
+        Text(
+            text = stringResource(resId),
+            modifier = Modifier
+                .padding(start = 8.dp)
+        )
+    }
 }
