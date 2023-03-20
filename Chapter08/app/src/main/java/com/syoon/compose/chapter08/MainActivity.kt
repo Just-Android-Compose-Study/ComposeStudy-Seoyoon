312d159dc7979f6b2f02738f4de4385046ace94b
seoyoon513 <syoonlee513@gmail.com>
2023-03-20 19:31:33 +0900

    [feat] #15 StateDemo 구현

--- Chapter08/app/src/main/java/com/syoon/compose/chapter08/MainActivity.kt
+++ Chapter08/app/src/main/java/com/syoon/compose/chapter08/MainActivity.kt
@@ -3,13 +3,17 @@ package com.syoon.compose.chapter08
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.syoon.compose.chapter08.ui.theme.Chapter08Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +21,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Chapter08Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                StateChangeDemo()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun StateChangeDemo() {
+    var toggled by remember {
+        mutableStateOf(false)
+    }
+    val color = if (toggled)
+        Color.White
+    else
+        Color.Red
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            toggled = !toggled
+        }) {
+            Text(
+               stringResource(id = R.string.toggle) 
+            )
+        }
+        Box(
+            modifier = Modifier
+                .padding(top = 32.dp)
+                .background(color = color)
+                .size(128.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Chapter08Theme {
-        Greeting("Android")
+        StateChangeDemo()
     }
 }
